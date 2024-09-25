2e3c868e0e859a547954f066be397ce7f0798c97
hippolyte42 <hippolyte.jacque@gmail.com>
2024-09-25 05:32:36 +0200

    getOpportunitiesUsecase: filter on dev cards + trim low prices

--- src/application/core/getOpportunities.usecase.ts
+++ src/application/core/getOpportunities.usecase.ts
@@ -1,3 +1,4 @@
+import { set } from "zod";
 import { CARDMARKET_FEE } from "../../constants";
 import { CardRepositoryPort } from "../../repository/ports/card.repository.port";
 
@@ -10,8 +11,72 @@ export class GetOpportunitiesUsecase {
       return [];
     }
     const opportunities = cards
+      .filter((card) =>
+        [
+          "Bazaar of Baghdad",
+          "Library of Alexandria",
+          "Diamond Valley",
+          "Guardian Beast",
+          "City of Brass",
+          "Drop of Honey",
+          "Shahrazad",
+          "Island of Wak-Wak",
+          "City in a Bottle",
+          "Erhnam Djinn",
+          "Old Man of the Sea",
+          "Elephant Graveyard",
+          "Singing Tree",
+          "Ifh-Bíff Efreet",
+          "King Suleiman",
+          "Khabál Ghoul",
+          "Merchant Ship",
+          "Mishra's Factory white",
+          "Transmute Artifact",
+          "Argivian Archaeologist",
+          "Triskelion",
+          "Tawnos's Coffin",
+          "Gate to Phyrexia",
+          "Citanul Druid",
+          "Hurkyl's Recall",
+          "Ivory Tower",
+          "Mishra's Factory green",
+          "Mishra's Workshop",
+          "Power Artifact",
+          "Powerleech",
+          "Shatterstorm",
+          "Strip Mine",
+          "Strip Mine",
+          "Strip Mine",
+          "Strip Mine",
+          "Su-Chi",
+          "Tetravus",
+          "Candelabra of Tawnos",
+          "All Hallow's Eve",
+          "Nether Void",
+          "The Abyss",
+          "Chains of Mephistopheles",
+          "Land Equilibrium",
+          "Mirror Universe",
+          "Acid Rain",
+          "The Tabernacle at Pendrell Vale",
+          "Living Plane",
+          "Goblin Wizard",
+          "Season of the Witch",
+          "Mana Vortex",
+          "Eater of the Dead",
+          "Exorcist",
+          "Martyr's Cry",
+          "Eternal Flame",
+          "Psychic Allergy",
+          "Uncle Istvan",
+        ].includes(card.name)
+      )
       .map((card) => {
-        if (!card.cardMarketPrice || !card.marketPrice) {
+        if (
+          !card.cardMarketPrice ||
+          !card.marketPrice ||
+          (card.cardMarketPrice && card.cardMarketPrice < 15)
+        ) {
           return;
         }
         const buyPrice = card.cardMarketPrice / (1 - CARDMARKET_FEE);
@@ -22,6 +87,7 @@ export class GetOpportunitiesUsecase {
         }
         return {
           cardName: card.name,
+          set: card.set,
           buyPrice,
           sellPrice,
           profit,
