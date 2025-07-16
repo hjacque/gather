dd0eda9d43d26f3689e09a8b4d866ab592de3a4f
Hippolyte Jacque <hippolyte.jacque@gmail.com>
2025-07-16 19:44:49 +0200

    migration: new pokemon x y block

---
+++ prisma/migrations/20250716172115_x_y_block/migration.sql
@@ -0,0 +1,2 @@
+-- AlterEnum
+ALTER TYPE "Block" ADD VALUE 'x_y';
--- src/types/block.ts
+++ src/types/block.ts
@@ -1,1 +1,1 @@
-export type Block = "scarlet_and_violet" | "sword_and_shield" | "sun_and_moon";
+export type Block = "scarlet_and_violet" | "sword_and_shield" | "sun_and_moon" | "x_y";
