1ba8cfcffead958cb634a898c1198c27007a91e2
Hippolyte Jacque <hippolyte.jacque@gmail.com>
2025-08-14 19:39:12 +0200

    fix(syncScheduler): single every hour

--- src/services/syncScheduler.ts
+++ src/services/syncScheduler.ts
@@ -34,7 +34,7 @@ export class SyncSchedulerService {
   }
 
   async scheduleSingles() {
-    for (let hours = 0; hours < 24; hours+= 4) {
+    for (let hours = 0; hours < 24; hours+= 1) {
       cron.schedule(
         `0 ${hours} * * *`,
         async () => {
@@ -45,12 +45,12 @@ export class SyncSchedulerService {
         },
       );
     }
-    console.log("📅 Sync scheduler started (runs every 4 hours)", { type: "single" });
+    console.log("📅 Sync scheduler started (runs every hour at 00)", { type: "single" });
   }
 
   async scheduleSealed() {
     for (let hours = 0; hours < 24; hours++) {
-      const minutesArr = hours % 4 === 0 ? ["50"] : ["10", "30", "50"];
+      const minutesArr = ["45"];
       for (const minutes of minutesArr) {
         cron.schedule(
           `${minutes} ${hours} * * *`,
@@ -65,7 +65,7 @@ export class SyncSchedulerService {
         );
       }
     }
-    console.log("📅 Sync scheduler started (runs every 20 minutes)", {
+    console.log("📅 Sync scheduler started (runs every hour at 45)", {
       type: ["booster_box", "collector_booster_box", "booster_bundle", "booster_box_18", "elite_trainer_box"],
     });
   }
