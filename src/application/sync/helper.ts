41d281c6e4723c378db44172a2fc65229d5f432e
Hippolyte Jacque <hippolyte.jacque@gmail.com>
2025-12-08 04:01:18 +0100

    fix(getEurToUsdRate): now fallback to default

--- src/application/sync/helper.ts
+++ src/application/sync/helper.ts
@@ -2,9 +2,10 @@ import { DEFAULT_USD_TO_EUR } from "../../constants";
 
 export const getEurToUsdRate = async (): Promise<number> => {
     try {
-        return fetch("https://api.frankfurter.dev/v1/latest?base=USD&symbols=EUR")
+        const res = await fetch("https://api.frankfurter.dev/v1/latest?base=USD&symbols=EUR")
               .then(res => res.json())
-              .then(data => data.rates.EUR) || DEFAULT_USD_TO_EUR;
+              .then(data => data.rates.EUR);
+        return res;
     } catch (error) {
         console.error("Failed to fetch EUR to USD exchange rate, using default value.", error);
         return DEFAULT_USD_TO_EUR;
